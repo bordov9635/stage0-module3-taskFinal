47fcbd31591b903b81ed0145abb324e7897c76dc
Siarhei <bordov9635@gmail.com>
2023-04-18 14:45:27 +0300

    commit1

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.beans.PropertyEditorSupport;
+
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double result = (temperatureCelsius * 9.0 / 5.0) + 32;
+        System.out.println(result);
     }
 }
